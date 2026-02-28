10a88aaaedb6888268e5ae6105022f311e3dfb40
laneunruh18-tech <laneunruh18@gmail.com>
2026-02-28 14:53:32 -0600

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "workforce-cache-v1";
+const CACHE_NAME = "workforce-cache-v2";
 const ASSETS = [
   "./",
   "./index.html",
@@ -8,9 +8,7 @@ const ASSETS = [
 ];
 
 self.addEventListener("install", (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
-  );
+  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)));
   self.skipWaiting();
 });
 
